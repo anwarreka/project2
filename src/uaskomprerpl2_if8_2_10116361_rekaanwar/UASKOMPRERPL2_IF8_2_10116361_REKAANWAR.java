6262eb8218b22bef88acd942ea35c25cd865c6a6
anwarreka <rekaanwar17@gmail.com>
2019-02-10 16:55:42 +0700

    project kedua

---
+++ src/uaskomprerpl2_if8_2_10116361_rekaanwar/UASKOMPRERPL2_IF8_2_10116361_REKAANWAR.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uaskomprerpl2_if8_2_10116361_rekaanwar;
+
+/**
+ *
+ * @author RSA
+
+HARI/TANGGAL : Kamis, 7 Februari 2019
+
+NO SOAL : 2
+
+KELAS : IF8
+
+NIM : 10110269
+
+NAMA : RIZKI ADAM KURNIAWAN
+ */
+public class UASKOMPRERPL2_IF8_2_10116361_REKAANWAR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+
+        //udh coba pake for tpi gk bisa
+//        int i,A,B,H,S ;
+//        A = 500;
+//        B = 100000;
+//        H = A+B;
+//        for (i=1; i<=5; i++,H++)
+//            System.out.println("Saldo di bulan ke-"+i+ ":" +H);
+        
+        
+        int[] bonus = new int[6];
+        bonus[1] = 10500;
+        bonus[2] = 11000;
+        bonus[3] = 11500;
+        bonus[4] = 10200;
+        bonus[5] = 12500;
+        for (int i = 1; i<=5; i++)
+            System.out.println("Saldo di bulan ke-"+i+ ":" +bonus[i]);
+        }
+}
+
+
